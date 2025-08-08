4ecb6b090af2994c770d864788aa3efac57e9b89
hjham <hjham@4MIHJ.local>
2025-08-08 09:43:27 +0900

    프로그래밍 언어 공부 커밋

---
+++ C/ex2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("Hello World!\n");
+    printf("안녕하세요!\n");
+    printf("\"저는 함회장\"입니다.\n");
+    printf("\\n은 줄 바꿈을 의미합니다.\n");
+    printf("\t탭\t테스트\n");
+    printf("역슬래시 : \\, 따옴표: \",  탭: \t\n");
+    return 0;
+}
---
+++ python/main.py
@@ -0,0 +1,7 @@
+string = 'Python Good'
+s = string[1]
+print(s)
+s = string[10]
+print(s)
+s = string[-1]
+print(s)
